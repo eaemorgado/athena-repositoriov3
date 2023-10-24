2acd3721650dafc9960eed54e92d9c68b308ffe1
Gabriel Morgado <eaemorgado@gmail.com>
2023-10-24 07:59:35 -0300

    database atualizado

--- config/database.sql
+++ config/database.sql
@@ -1,19 +1,4 @@
 
--- DROP DATABASE IF exists `athenashop` ;
--- CREATE DATABASE `athenashop`;
--- USE `athenashop`;
-
--- DROP TABLE IF EXISTS `usuarios`;
-
--- CREATE TABLE usuarios (
---   id int primary key auto_increment,
---   nome varchar(255) NOT NULL,
---   email varchar(60) NOT NULL,
---   senha longtext NOT NULL,
---   telefone varchar(20) DEFAULT NULL
--- );
-
-
 -- DROP DATABASE `athenashop` ;
 CREATE DATABASE `athenashop`;
 USE `athenashop`;
@@ -28,6 +13,15 @@ CREATE TABLE usuarios (
   id_tipo_usuario int not null default '1'
 );
 
+CREATE TABLE produtos (
+  id varchar(200) primary key,
+  nome varchar(255) NOT NULL,
+  descricao varchar(255) NOT NULL,
+  quantidade varchar (60),
+  cores varchar (60),
+  preco varchar (60)
+);
+
 create table `tipo_usuario` (
 	id_tipo_usuario int not null auto_increment,
 	tipo_usuario varchar (25) default null,
@@ -37,6 +31,8 @@ create table `tipo_usuario` (
     
 );	
 
+
+
 alter table usuarios
 add constraint tipo_usuario
 foreign key (`id_tipo_usuario`) references tipo_usuario(`id_tipo_usuario`);
@@ -44,14 +40,5 @@ foreign key (`id_tipo_usuario`) references tipo_usuario(`id_tipo_usuario`);
 INSERT INTO `athenashop`.`tipo_usuario` (`id_tipo_usuario`, `tipo_usuario`, `inscricao_usuario`, `status_tipo_usuario`) VALUES ('1', 'cliente', 'usuario logado', '1');
 INSERT INTO `athenashop`.`tipo_usuario` (`id_tipo_usuario`, `tipo_usuario`, `inscricao_usuario`, `status_tipo_usuario`) VALUES ('2', 'vendedor', 'vendedor', '1');
 INSERT INTO `athenashop`.`tipo_usuario` (`id_tipo_usuario`, `tipo_usuario`, `inscricao_usuario`, `status_tipo_usuario`) VALUES ('3', 'adm', 'adm', '1');
-
-
-
-
-
-
-
-
-
 
 
